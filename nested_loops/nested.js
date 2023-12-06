3384dd49cd02ba9a6131479585f59f1b33a502e2
Marvel <dreelceejay79@yahoo.com>
2023-12-06 13:10:32 +0100

    6/12/2023

--- nested_loops/nested.js
+++ nested_loops/nested.js
@@ -1,12 +1,54 @@
-let students = ['Jack', 'Sarah', 'Micheal', 'Jane']
+const staffsDetails = [
+    { 
+    name: "Sam Josh",
+     age: 44,
+     salary: 4000,
+     currency: "USD",
+     image: "/assets/man1.png"
+    },
+
+    { 
+    
+    name: "Justina Kap",
+     age: 34,
+     salary: 3000,
+     currency: "USD" ,
+     image: "/assets/woman1.png"
+    },
+
+    { 
+    name: "Chris Colt",
+     age: 37,
+     salary: 3700,
+     currency: "USD" ,
+     image: "/assets/man2.png"
+    },
+
+    { 
+    name: "Jane Doe",
+     age: 24,
+     salary: 4200,
+     currency: "USD" ,
+     image: "/assets/woman2.png"
+    }
+
+  ];
 
 document.addEventListener('DOMContentLoaded', function(){
-    header =  document.querySelector('ul')
+    let names =  document.querySelector('ul')
 
-    students.forEach(function(student) {
-        li = document.createElement('li')
-        li.innerHTML = student
-        header.append(li)
+    staffsDetails.forEach(function(currentElement) {
+         let li = document.createElement('li')
+        li.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="${currentElement.image}" class="card-img-top" alt="avatar">
+        <div class="card-body">
+          <h5 class="card-title">Name: ${currentElement.name}</h5>
+          <h6>Age: ${currentElement.age} </h6>
+          <p class="card-text">Salary: $ ${currentElement.salary}</p>
+          <a href="#" class="btn btn-primary"> Call ${currentElement.name}</a>
+        </div>
+      </div>`
+        names.append(li)
         
     })
 })
