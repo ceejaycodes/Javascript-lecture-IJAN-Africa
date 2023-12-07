0f9951a9d0ef50865a49d3e6cad173bf8146957e
Marvel <dreelceejay79@yahoo.com>
2023-12-07 13:56:43 +0100

    07/12/2023

--- nested_loops/nested.js
+++ nested_loops/nested.js
@@ -35,5 +35,19 @@ const staffsDetails = [
   ];
 
 document.addEventListener('DOMContentLoaded', function(){
+  let Unordered = document.querySelector('ul')
+  staffsDetails.forEach(current => {
+    let listElement = document.createElement('li')
+    listElement.innerHTML = `<div class="card" style="width: 18rem;">
+    <img src="${current.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
+    Unordered.append(listElement)
+    
+  })
   
 })
